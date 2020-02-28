a86ca057ab30c8f2a2448522ff0141965b9646cc
Celine <celine.desnoe@weloop.io>
2020-02-28 17:35:50 +0100

    [ADD] login

--- bacchus-server/routes/auth-router.js
+++ bacchus-server/routes/auth-router.js
@@ -40,47 +40,47 @@ router.post("/process-signup", (req, res, next) => {
 // //          LOGIN
 // // ************************
 
-// router.post("/process-login", (req, res, next) => {
-//   const { email, originalPassword } = req.body;
+router.post("/process-login", (req, res, next) => {
+  const { email, originalPassword } = req.body;
 
-//   // validate the email by searching the database for an account with that email
-//   User.findOne({ email: { $eq: email } })
-//     .then(userDoc => {
-//       // User.findOne() will give us NULL in userDoc if it found nothing
-//       if (!userDoc) {
-//         // req.flash() sends a feedback message before a redirect
-//         // (it's defined by the "connect-flash" npm package)
-//         next(new Error("Email is incorrect."));
+  // validate the email by searching the database for an account with that email
+  User.findOne({ email: { $eq: email } })
+    .then(userDoc => {
+      // User.findOne() will give us NULL in userDoc if it found nothing
+      if (!userDoc) {
+        // req.flash() sends a feedback message before a redirect
+        // (it's defined by the "connect-flash" npm package)
+        next(new Error("Email is incorrect."));
 
-//         return;
-//       }
+        return;
+      }
 
-//       const { encryptedPassword } = userDoc;
+      const { encryptedPassword } = userDoc;
 
-//       // validate the password by using bcrypt.compareSync()
-//       // (bcrypt.compareSync() will return FALSE if the passwords don't match)
-//       if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
-//         // req.flash() sends a feedback message before a redirect
-//         // (it's defined by the "connect-flash" npm package)
-//         next(new Error("Password is incorrect"));
-//         return;
-//       }
+      // validate the password by using bcrypt.compareSync()
+      // (bcrypt.compareSync() will return FALSE if the passwords don't match)
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        // req.flash() sends a feedback message before a redirect
+        // (it's defined by the "connect-flash" npm package)
+        next(new Error("Password is incorrect"));
+        return;
+      }
 
-//       // email & password are CORRECT!
-//       // if we MANUALLY managed the user session
-//       // req.session.userId = userDoc._id;
+      // email & password are CORRECT!
+      // if we MANUALLY managed the user session
+      // req.session.userId = userDoc._id;
 
-//       // instead we'll use PASSPORT – an npm package for managing user sessions
-//       // req.logIn() is a Passport method that calls serializeUser()
-//       // (that saves the USER ID in the session which means we are logged-in)
-//       req.logIn(userDoc, () => {
-//         // hide encrypted password before sending the JSON (it's a security risk)
-//         userDoc.encryptedPassword = undefined;
-//         res.json(userDoc);
-//       });
-//     })
-//     .catch(err => next(err));
-// });
+      // instead we'll use PASSPORT – an npm package for managing user sessions
+      // req.logIn() is a Passport method that calls serializeUser()
+      // (that saves the USER ID in the session which means we are logged-in)
+      req.logIn(userDoc, () => {
+        // hide encrypted password before sending the JSON (it's a security risk)
+        userDoc.encryptedPassword = undefined;
+        res.json(userDoc);
+      });
+    })
+    .catch(err => next(err));
+});
 
 // // ************************
 // //          LOGOUT
